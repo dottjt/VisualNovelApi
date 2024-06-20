b3adb36b7b4ad460304b9c13e26681090f76b5ad
Julius Reade <julius.reade@gmail.com>
2024-06-21 09:08:14 +1000

    feat: add an incomplete novel controller

---
+++ Controllers/NovelController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using VisualNovelApi.Model;
+
+namespace VisualNovelApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NovelController : ControllerBase
+    {
+        public NovelController()
+        {
+        }
+
+        // [HttpGet("")]
+        // public ActionResult<IEnumerable<TModel>> GetTModels()
+        // {
+        //     return new List<TModel> { };
+        // }
+        //
+        // [HttpGet("{id}")]
+        // public ActionResult<TModel> GetTModelById(int id)
+        // {
+        //     return null;
+        // }
+        //
+        // [HttpPost("")]
+        // public ActionResult<TModel> PostTModel(TModel model)
+        // {
+        //     return null;
+        // }
+        //
+        // [HttpPut("{id}")]
+        // public IActionResult PutTModel(int id, TModel model)
+        // {
+        //     return NoContent();
+        // }
+        //
+        // [HttpDelete("{id}")]
+        // public ActionResult<TModel> DeleteTModelById(int id)
+        // {
+        //     return null;
+        // }
+    }
+}
