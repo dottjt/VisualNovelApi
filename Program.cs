ed0cd02152b0e611d3f3503d729eda2eead7c900
Julius Reade <julius.reade@gmail.com>
2024-06-20 20:02:22 +1000

    feat: add new packages/organise nswag

--- Program.cs
+++ Program.cs
@@ -1,24 +1,53 @@
 var builder = WebApplication.CreateBuilder(args);
 
+const string allowDevelopmentCorsOriginsPolicy = "AllowDevelopmentSpecificOrigins";
+const string localDevelopmentUrl = "http://localhost:4200";
+
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: allowDevelopmentCorsOriginsPolicy,
+        builder =>
+        {
+            builder.WithOrigins(localDevelopmentUrl)
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials();
+        });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerDocument();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+    // app.UseSwagger();
+    // app.UseSwaggerUI();
 }
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseOpenApi();
+app.UseSwaggerUi();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors(allowDevelopmentCorsOriginsPolicy);
+}
 
 app.MapControllers();
 
